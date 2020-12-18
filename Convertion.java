a41299f7fa65f4b9580dc90c623e4db3f4fe2040
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-18 12:24:51 +0530

    Add files via upload

---
+++ Convertion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Convertion
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+         int chr = 'Z';
+        System.out.println("The ASCII Value  Of Z  is  _" + chr);
+}
+}
