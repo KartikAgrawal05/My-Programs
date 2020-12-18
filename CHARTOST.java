45adb9814d6f4ad7beb6d78736059ff5d61a0432
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-18 12:27:58 +0530

    Add files via upload

---
+++ CHARTOST.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CHARTOST
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+           System.out.println("String to char");
+             System.out.println("The char is-" +  'c');
+        char ch = 'c' ;
+        String st = Character .toString(ch);
+        System.out.println("The String is-" +  st );
+}
+}
