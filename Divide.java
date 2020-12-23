fef37853b413623f1ba28ae1d73cd37f1ea4ddc3
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-23 12:33:31 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Divide
+{
+    public static void main(String[]args)
+{
+    int x =12;
+    int y =3;
+    int z ;
+    z = x /y;
+    System.out.println( "When we Divide 12 and 3 = "+z  );
+} 
+
+}
