d01e4f34ace60de8aa2b1998d92b4e7dea4a0ab8
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-23 12:35:35 +0530

    Add files via upload

---
+++ InputAdd.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class InputAdd
+{
+    public static void main(String[]args)
+{ 
+    Scanner read=new Scanner(System.in);
+   
+int x=read.nextInt();
+    
+   
+    int y =read.nextInt();
+    int z ;
+    z = x +y;
+    System.out.println( "When we Add both = "+z  );
+} 
+
+}
