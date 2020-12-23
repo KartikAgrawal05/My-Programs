ba500330431ea632f5e4800446880d726f1fd446
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-23 12:27:42 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Add
+{
+    public static void main(String[]args)
+{
+    int x =2;
+    int y =3;
+    int z ;
+    z = x +y;
+    System.out.println( "When we Add 2 and 3 = "+z  );
+} 
+
+}
