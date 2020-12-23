9a91fc8a75865609e4ca1cbded395d978683828e
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-23 12:29:08 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Subtract
+{
+    public static void main(String[]args)
+{
+    int x =5;
+    int y =3;
+    int z ;
+    z = x -y;
+    System.out.println( "When we Sub. 5 and 3 = "+z  );
+} 
+
+}
