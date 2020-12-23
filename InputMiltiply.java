4b01aa79a6e552438378731a456588f10544a2ff
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-23 13:00:32 +0530

    Add files via upload

---
+++ InputMiltiply.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class InputMiltiply
+{
+    public static void main(String[]args)
+{ 
+    Scanner read=new Scanner(System.in);
+   System.out.println("Enter the 1 no ");
+    int x=read.nextInt();
+     System.out.println("Enter the 2 no ");
+    int y =read.nextInt();
+    int z ;
+    z = x *y;
+    System.out.println( "When we Multiply both = "+z  );
+} 
+
+}
