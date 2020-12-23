d7ccb096f32802bb7a629fb4af148a29408ef3c7
Kartik Agrawal <76148944+KartikAgrawal05@users.noreply.github.com>
2020-12-23 12:30:59 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Multiply
+{
+    public static void main(String[]args)
+{
+    int x =2;
+    int y =3;
+    int z ;
+    z = x -y;
+    System.out.println( "When we Multiply 2 and 3 = "+z  );
+} 
+
+}
